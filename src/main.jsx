28ed4f42501e5a7409f097c6c754f3a9fbc63b57
Hamad Pervaiz <hamad@bearplex.com>
2026-02-22 16:19:44 +0500

    Add password protection and block search engine indexing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PasswordGate from './components/PasswordGate.jsx'
 import { ThemeProvider } from './context/ThemeContext'
+
+const HASH = '396c5323cba706a94f6bcb6ac71d0dc2d309ae92604f1949780c41be5e318c5e'
+
+function Root() {
+  const [authed, setAuthed] = useState(() => sessionStorage.getItem('r2t-auth') === HASH)
+
+  return authed ? <App /> : <PasswordGate onUnlock={() => setAuthed(true)} />
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <Root />
     </ThemeProvider>
   </StrictMode>,
 )
